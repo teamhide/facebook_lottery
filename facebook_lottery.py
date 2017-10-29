f9da991163a31e39b83b1d44b0d04be22b867d9b
Hide <padocon@naver.com>
2017-10-29 20:38:37 +0900

    Update facebook_lottery.py

--- facebook_lottery.py
+++ facebook_lottery.py
@@ -1,13 +1,14 @@
 import json
 import requests
-from random import *
 import time
+import random
 
-user = []
-post = "" # Post number(page_post)
-token = "" # Your access token
-limit = "5000"
+user = {}
+post = "" # 페이지번호_포스트번호 형태
+token = "" # access_token
+limit = "5000" # 가져올 댓글의 수
 
+print("[*] 추첨을 위한 댓글 수집을 시작합니다.")
 r = requests.get("https://graph.facebook.com/"+post+"/comments/?access_token="+token+"&limit="+limit)
 r.encoding = "utf8"
 
@@ -17,14 +18,17 @@ count = str(len(decode_str['data']))
 print("[*] 총 "+count+"개의 댓글을 가져왔습니다.")
 
 for i in range(int(count)):
-	user.append(decode_str['data'][i]['from']['name']+decode_str['data'][i]['from']['id'])
+	user[decode_str['data'][i]['from']['name']+decode_str['data'][i]['from']['id']] = decode_str['data'][i]['from']['id']+"|"+decode_str['data'][i]['message']
 
-print("[*] 중복 댓글 제거를 시작합니다.")
-remove_dup = list(set(user))
-print("[*] 총 댓글 수 : "+str(len(remove_dup)))
-seed = randrange(int(count))
-print("[*] 당첨자 추첨을 위한 시드 값을 생성했습니다 - > "+str(seed))
+print("[*] 중복 제거 후 총 댓글 수 : "+str(len(user)))
+seed = random.choice(list(user.keys()))
+print("[*] 당첨자 추첨을 위한 시드 값을 생성했습니다.")
 print("-------------------------------------------------------")
-time.sleep(1)
+info = user[seed].split('|')
+url = "https://www.facebook.com/app_scoped_user_id/"+info[0]+"/"
+comment = info[1]
+time.sleep(3)
 print("[*] 추첨 결과")
-print(remove_dup[seed])
+print("이름 : "+seed)
+print("주소 : "+url)
+print("댓글 : "+comment)
